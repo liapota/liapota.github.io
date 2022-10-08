8bbbef26e44b9af9d8c75fd3e9cb8516398b87d6
Александр <34779498+Canek4005@users.noreply.github.com>
2022-10-08 10:45:29 +0300

    add tabs

--- project/frontend/src/components/MainTabs/MainTabs.jsx
+++ project/frontend/src/components/MainTabs/MainTabs.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { observer } from "mobx-react-lite";
-import { MainTabsWrapper } from "./MainTabs.styles";
+import { MainTabsWrapper, Tab, Tabs, TabsWrapper } from "./MainTabs.styles";
 
 const MainTabs = () => {
+  const [active, setActive] = useState(1);
   return (
     <>
       <MainTabsWrapper>
-        <div>Главные переключатели</div>
+        <TabsWrapper>
+          <Tab active={0 === active} onClick={()=>setActive(0)}>МАГАЗИН</Tab>
+          <Tab active={1 === active} onClick={()=>setActive(1)}>ЛЕНТА</Tab>
+          <Tab active={2 === active} onClick={()=>setActive(2)}>ПРОФИЛЬ</Tab>
+        </TabsWrapper>
       </MainTabsWrapper>
     </>
   );
--- project/frontend/src/components/MainTabs/MainTabs.styles.js
+++ project/frontend/src/components/MainTabs/MainTabs.styles.js
@@ -2,7 +2,32 @@ import styled from "styled-components";
 
 export const MainTabsWrapper = styled.div`
   width: 100%;
-  background-color: aqua;
+  height: 100%;
   display: flex;
   justify-content: center;
+  max-height: 75px;
+`;
+
+export const TabsWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 820px;
+`;
+
+export const Tab = styled.div`
+cursor: pointer;
+  border-radius: 10px;
+  height: 100%;
+  align-items: center;
+  width: 100%;
+  background-color: #b7c0dc;
+  text-align: center;
+
+  background-color: ${(props) => (props?.active ? "white" : "#B7C0DC")};
+  color: ${(props) => (props?.active ? "#00ABFF" : "#092896")};
+  min-height: ${(props) => (props?.active ? "75px" : "60px")};
+  font-size: ${(props) => (props?.active ? "36px" : "32px")};
 `;
