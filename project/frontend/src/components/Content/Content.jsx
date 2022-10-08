966ac9af0bfb4b560d70da7a9711c51a4bb07157
Александр <34779498+Canek4005@users.noreply.github.com>
2022-10-08 23:34:43 +0300

    try fix cors

--- project/frontend/src/components/Content/Content.jsx
+++ project/frontend/src/components/Content/Content.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { observer } from "mobx-react-lite";
 import UserSummary from "../UserSummary";
 import MainTabs from "../MainTabs";
 import Body from "../Body";
 import { ContentWrapper } from "./Content.styles";
-
+import { useStore } from "../../store";
 
 const Content = () => {
+  const { userStore, usersListStore } = useStore();
+  const { getUserHandler } = userStore;
+  const { getUsersHandler } = usersListStore;
+
+  useEffect(() => {
+    getUserHandler(1);
+    getUsersHandler();
+  }, []);
+
   return (
     <ContentWrapper>
       <UserSummary />
