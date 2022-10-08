f2d73873ebfd4d4020f5e0eca68e33381fcb58c1
polina.t <polina.t@ati.su>
2022-10-08 21:52:40 +0300

    main

---
+++ project/frontend/src/components/Body/Display/Profile/ProfileComponents/NFTComponents.styes.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CardWrapper = styled.div`
+    width: 200px;
+    height: 200px;
+    border: 5px solid rgba(9, 40, 150, 1);
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-weight: bold;
+    margin: 40px;
+`
